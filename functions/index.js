78b31ad804ca17c6addcd293335be0788d00edc6
Michael Schilling <mschilling@arkid.nl>
2017-10-27 16:11:28 +0200

    Refactore Firebase Functions, deprecating TypeScript support :-(

--- functions/index.js
+++ functions/index.js
@@ -1,184 +1,54 @@
-(function(e, a) { for(var i in a) e[i] = a[i]; }(this, /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports) {
+'use strict';
 
-module.exports = require("firebase-functions");
+process.env.DEBUG = 'actions-on-google:*';
 
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-module.exports = require("firebase-admin");
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/index.ts
-const functions = __webpack_require__(0);
-const admin = __webpack_require__(1);
-const EchoService = __webpack_require__(3);
-const Assistant = __webpack_require__(4);
+const Assistant = require('actions-on-google').ApiAiAssistant;
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-exports.echo = EchoService.listener;
-exports.assistant = Assistant.listener;
 
+const people = admin.firestore().collection('people');
 
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
+// API.AI Intent names
+const TWITTER_HANDLE_INTENT = 'twitter-handle';
 
-"use strict";
+// // Contexts
+const WELCOME_CONTEXT = 'welcome';
+const TWITTER_HANDLE_CONTEXT = 'twitter-handle';
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+// Context Parameters
+const PERSON_PARAM = 'person';
+
+exports.assistant = functions.https.onRequest((request, response) => {
+  console.log('headers: ' + JSON.stringify(request.headers));
+  console.log('body: ' + JSON.stringify(request.body));
+
+  const assistant = new Assistant({request: request, response: response});
+
+  const actionMap = new Map();
+  actionMap.set(TWITTER_HANDLE_INTENT, twitterHandle);
+  assistant.handleRequest(actionMap);
+
+  function twitterHandle(assistant) {
+    const person = assistant.getArgument(PERSON_PARAM);
+    console.log(`Person: ${person}`);
+
+    people.doc(person).get().then(function(doc) {
+      if (doc.exists) {
+        const data = doc.data();
+
+        const speech = `<speak>
+          ${data.first_name}'s twitter handle is @${data.twitter}<break time="2"/>
+          Anything else?
+          </speak>`;
+        assistant.ask(speech);
+      } else {
+        const speech = 'Sorry, I couldn\'t find any Twitter handle for ' + person;
+        assistant.ask(speech);
+      }
     });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = __webpack_require__(0);
-const admin = __webpack_require__(1);
-exports.listener = functions.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
-    let payload = {
-        method: req.method,
-        headers: req.headers,
-        body: req.body
-    };
-    const snapshot = yield admin.database().ref('/echo-service').push(payload);
-    if (req.body.text && !req.body.bot_id) {
-        res.send({ text: req.body.text });
-    }
-    else {
-        res.send({});
-    }
-}));
-
-
-/***/ }),
-/* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = __webpack_require__(0);
-const actions_on_google_ts_1 = __webpack_require__(5);
-// import { Response.RichResponse } from 'actions-on-google-ts'
-/**
- * The entry point to handle a http request
- * @param {Request} request An Express like Request object of the HTTP request
- * @param {Response} response An Express like Response object to send back data
- */
-exports.listener = functions.https.onRequest((request, response) => __awaiter(this, void 0, void 0, function* () {
-    console.log(`Request headers: ${JSON.stringify(request.headers)}`);
-    console.log(`Request body: ${JSON.stringify(request.body)}`);
-    const app = new actions_on_google_ts_1.ApiAiApp({ request, response });
-    /*
-    const actionMap = new Map();
-    actionMap.set(app.StandardIntents.MAIN, () => {
-        const richResponse: any = app.buildRichResponse()
-          .addSimpleResponse('Hello Webhook')
-          .addSuggestions(['foo', 'bar']);
-        app.ask(richResponse);
-  
-    });
-    app.handleRequest(actionMap);
-  */
-    const richResponse = app.buildRichResponse()
-        .addSimpleResponse('Hello Webhook')
-        .addSuggestions(['foo', 'bar']);
-    app.ask(richResponse);
-    // const app = new ApiAiApp({request, response});
-    // const actionMap = new Map<string, RequestHandler>();
-    // // app.handleRequest(actionMap);
-    // app.ask('How do you mean?')
-}));
-
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports) {
-
-module.exports = require("actions-on-google-ts");
-
-/***/ })
-/******/ ])));
+    // const parameters = {};
+    // parameters[PERSON_PARAM] = true;
+    // assistant.setContext(LEARN_THING_CONTEXT, 2, parameters);
+  }
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,10 @@
-// src/index.ts
-import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
-import * as EchoService from './echo-service'
-import * as Assistant from './assistant'
+// // src/index.ts
+// import * as functions from 'firebase-functions'
+// import * as admin from 'firebase-admin'
+// import * as EchoService from './echo-service'
+// import * as Assistant from './assistant'
 
-admin.initializeApp(functions.config().firebase)
+// admin.initializeApp(functions.config().firebase)
 
-export const echo = EchoService.listener
-export const assistant = Assistant.listener
+// export const echo = EchoService.listener
+// export const assistant = Assistant.listener
