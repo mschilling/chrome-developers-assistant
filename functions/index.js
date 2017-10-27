9d5a5de8ce59fa46586350c34882eec0c79a1c7c
Michael Schilling <mschilling@arkid.nl>
2017-10-27 16:29:13 +0200

    Refactor webhook handling for Intent Twitter handle

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const people = admin.firestore().collection('people');
+const TwitterHandleIntent = require('./get-twitter-handle');
 
 // API.AI Intent names
 const TWITTER_HANDLE_INTENT = 'twitter-handle';
@@ -16,9 +16,6 @@ const TWITTER_HANDLE_INTENT = 'twitter-handle';
 const WELCOME_CONTEXT = 'welcome';
 const TWITTER_HANDLE_CONTEXT = 'twitter-handle';
 
-// Context Parameters
-const PERSON_PARAM = 'person';
-
 exports.assistant = functions.https.onRequest((request, response) => {
   console.log('headers: ' + JSON.stringify(request.headers));
   console.log('body: ' + JSON.stringify(request.body));
@@ -26,29 +23,6 @@ exports.assistant = functions.https.onRequest((request, response) => {
   const assistant = new Assistant({request: request, response: response});
 
   const actionMap = new Map();
-  actionMap.set(TWITTER_HANDLE_INTENT, twitterHandle);
+  actionMap.set(TWITTER_HANDLE_INTENT, TwitterHandleIntent.twitterHandle);
   assistant.handleRequest(actionMap);
-
-  function twitterHandle(assistant) {
-    const person = assistant.getArgument(PERSON_PARAM);
-    console.log(`Person: ${person}`);
-
-    people.doc(person).get().then(function(doc) {
-      if (doc.exists) {
-        const data = doc.data();
-
-        const speech = `<speak>
-          ${data.first_name}'s twitter handle is @${data.twitter}<break time="2"/>
-          Anything else?
-          </speak>`;
-        assistant.ask(speech);
-      } else {
-        const speech = 'Sorry, I couldn\'t find any Twitter handle for ' + person;
-        assistant.ask(speech);
-      }
-    });
-    // const parameters = {};
-    // parameters[PERSON_PARAM] = true;
-    // assistant.setContext(LEARN_THING_CONTEXT, 2, parameters);
-  }
 });
