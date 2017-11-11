69d04610b305500e80b352739faf831fc103bcf4
Michael Schilling <mschilling@arkid.nl>
2017-11-11 22:37:50 +0100

    Fix event highlights query based on tags

--- functions/helpers/api/videos/index.js
+++ functions/helpers/api/videos/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const Debug = require('debug');
-const debug = Debug('bite-api:debug');
-const error = Debug('bite-api:error');
+const debug = Debug('google-developer-assistant-api:debug');
+const error = Debug('google-developer-assistant-api:error');
 
 const admin = require('firebase-admin');
 const videosRef = admin.firestore().collection('videos');
@@ -23,17 +23,21 @@ function searchKeynoteVideos(eventName, year, limit = 3) {
 
 function searchEventHighlightsVideo(eventKey) {
   if (!eventKey) {
+    debug('eventKey is undefined');
     return undefined;
   };
 
   return videosRef
     .where('eventKey', '==', eventKey)
+    .where('tags.highlights', '==', true)
     .limit(1)
     .get()
     .then(snapshot => {
+      debug('videosRef snapshot result', snapshot.docs);
       if (snapshot.docs.length > 0) {
-          return snapshot.docs[0].data();
+        return snapshot.docs[0].data();
       }
+      debug('no results found');
       return undefined;
     });
 }
