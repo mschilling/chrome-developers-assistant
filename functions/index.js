4486de24ae8f0ac70b222a1eb9431ecbe7e53804
Michael Schilling <mschilling@arkid.nl>
2017-11-19 09:53:44 +0100

    Update actions-on-google dependency + remove depricated constructor

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 
 process.env.DEBUG = 'actions-on-google:*';
 
-const Assistant = require('actions-on-google').ApiAiAssistant;
+const Assistant = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
