bf10ba7a6c6c64003f26a038b5268683c713f584
Michael Schilling <mschilling@arkid.nl>
2017-12-11 10:10:10 +0100

    Add assistant-actions (enum) file

---
+++ functions/assistant-actions.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  ACTION_WELCOME: 'input.welcome',
+  ACTION_VIDEO_SEARCH: 'video-search',
+  ACTION_VIDEO_SEARCH_FALLBACK: 'option.select',
+  ACTION_NEXT_EVENT: 'next-event',
+  ACTION_PREV_EVENT: 'prev-event',
+  ACTION_SPEAKER_INFO: 'speaker-info',
+  ACTION_SPEAKER_INFO_KNOW_FOR: 'speaker-info.known-for',
+  ACTION_SPEAKER_SELECTION: 'select-speaker',
+  ACTION_SPEAKER_SELECT_FALLBACK: 'option.select-speaker',
+  ACTION_BLOGPOST_SEARCH: 'blogpost-search'
+};
