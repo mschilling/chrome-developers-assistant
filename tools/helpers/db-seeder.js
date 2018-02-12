556ad6f55acb5fb7c328c25eb0da5f6d87af7760
Michael Schilling <mschilling@arkid.nl>
2018-02-12 08:15:30 +0100

    Extend person model (googlplus + medium profile)

--- tools/helpers/db-seeder.js
+++ tools/helpers/db-seeder.js
@@ -33,12 +33,15 @@ class DbLoader {
         github: asFbString(row.githubhandle),
         lanyrd: asFbString(row.lanyrdhandle),
         youtube: asFbString(row.youtubehandle),
+        google_plus: asFbString(row.googleplus),
+        medium: asFbString(row.medium),
         speaker_deck: asFbString(row.speakerdeck),
         bio: asFbString(row.bio),
         short_bio: asFbString(row.shortbio),
         email: asFbString(row.email),
         pictureUrl: asFbString(row.pictureurl),
         homepage: asFbString(row.homepage),
+        rss_feed: asFbString(row.rssfeed),
         is_speaker: asFbBool(row.isspeaker)
       };
       actions.push(dbRef.doc(docId).set(obj, { merge: true }));
