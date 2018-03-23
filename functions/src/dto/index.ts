ab5fa583360a2c5ece84de51ecd599c966fa8f24
Michael Schilling <mschilling@arkid.nl>
2018-03-23 22:17:00 +0100

    Add People DTO class

--- functions/src/dto/index.ts
+++ functions/src/dto/index.ts
@@ -3,6 +3,7 @@ const debug = Debug('google-developer-assistant-api:debug');
 const error = Debug('google-developer-assistant-api:error');
 
 import { VideosRepository } from './videos';
+import { PeopleRepository } from './people';
 
 // Configure logging for hosting platforms that only support console.log and console.error
 debug.log = console.log.bind(console);
@@ -11,5 +12,7 @@ error.log = console.error.bind(console);
 export class DTO {
 
   static Videos = VideosRepository;
+
+  static People = PeopleRepository
 
 }
---
+++ functions/src/dto/people.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+import * as admin from 'firebase-admin';
+
+// const Debug = require('debug');
+// const debug = Debug('google-developer-assistant-api:debug');
+// const error = Debug('google-developer-assistant-api:error');
+
+const peopleRef = admin.firestore().collection('people');
+
+export class PeopleRepository {
+
+  static GetPeople(limit = 10) {
+    return peopleRef
+      .orderBy('rank', 'desc')
+      .limit(limit)
+      .get()
+      .then(snapshot => {
+        const docs = [];
+        for (let i = 0; i < snapshot.docs.length; i++) {
+          docs.push(snapshot.docs[i].data());
+        }
+        return docs;
+      });
+  }
+
+  static GetPerson(id) {
+    return peopleRef
+      .doc(id)
+      .get()
+      .then(snapshot => {
+        return snapshot.data();
+      });
+  }
+}
