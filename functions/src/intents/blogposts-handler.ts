a481ba96a58a410a66f39c75296b303e1e01ac1e
Michael Schilling <mschilling@arkid.nl>
2018-05-21 21:35:10 +0200

    Finalize intent handler for Search Blog Posts

--- functions/src/intents/blogposts-handler.ts
+++ functions/src/intents/blogposts-handler.ts
@@ -10,18 +10,16 @@ export async function searchBlogPosts(conv, params) {
   if (person) {
     filters.person = person;
   }
-  console.log('searchBlogPosts', person);
 
   const results = await api.searchBlogPosts(filters, 10);
   if (results && results.length > 0) {
     const result = results[0];
     if (results.length > 1) {
-      return await responses.returnBlogPostsResponse(conv, true, results);
+      responses.returnBlogPostsResponse(conv, true, results);
     } else {
-      return await responses.returnBasicCard(conv, 'blogpost', result);
+      responses.returnBasicCard(conv, 'blogpost', result);
     }
   } else {
     conv.ask(Str.DEFAULT_NO_RESULT.TEXT);
   }
-  return;
 }
