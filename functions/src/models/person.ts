dcf4f941ebe13da6b98ed627cf17a4da02af8ae8
Michael Schilling <mschilling@arkid.nl>
2018-06-16 21:02:25 +0200

    Update Person model

--- functions/src/models/person.ts
+++ functions/src/models/person.ts
@@ -1,3 +1,20 @@
 export class Person {
-  // uid: string;
+  id: string;
+  first_name: string;
+  last_name: string;
+  email?: string;
+  github?: string;
+  bio?: string;
+  google_plus?: string;
+  homepage?: string;
+  lanyrd?: string;
+  is_speaker?: boolean;
+  medium?: string;
+  pictureUrl: string;
+  rank: number;
+  rss_feed?: string;
+  short_bio?: string;
+  speaker_deck?: string;
+  twitter?: string;
+  youtube?: string;
 }
