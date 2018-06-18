f086892a912edcbc9700f1fc5d817e122dcb0986
Michael Schilling <mschilling@arkid.nl>
2018-06-18 21:26:33 +0200

    Extend card (with option type and value properties; update video and people services

--- functions/src/models/card.ts
+++ functions/src/models/card.ts
@@ -1,6 +1,9 @@
 export class GenericCard {
   _id: string;
+  _optionType: string;
+  _optionValue: string;
   title: string;
+  subTitle: string;
   description: string;
   imageUrl: string;
   imageAlt: string;
