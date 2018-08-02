16f18c76a940ba2e5dcc8aa3f66023704c187755
Michael Schilling <mschilling@arkid.nl>
2018-08-02 15:46:04 +0200

    Remove intent logging in speakers module

--- functions/src/prompts/speakers/module.ts
+++ functions/src/prompts/speakers/module.ts
@@ -4,13 +4,12 @@ import { speakerVideosIntent, speakerIntent } from './intents/speaker-intent';
 
 const intents = {
   'speaker_intent': speakerIntent,
-  'speaker_videos_intent': speakerVideosIntent,
-  'speaker_known_for_intent': knownForHandler,
+  'speaker_followup_videos_intent': speakerVideosIntent,
+  'speaker_followup_known_for_intent': knownForHandler,
   'Speakers - Speaker info': speakerIntent,
   'browse_speakers_intent': browseSpeaker,
 };
 
 export const module = (conv, ...args): any => {
-  console.log('conv.intent', conv.intent, new Date() );
   return intents[conv.intent](conv, ...args);
 };
