4ab4717a72c91f942640b7153d61ccdd961a927d
Michael Schilling <mschilling@arkid.nl>
2018-08-02 21:49:20 +0200

    Rename speaker parameter for browse blog event

--- functions/src/prompts/blogs/intents/browse-blogs.ts
+++ functions/src/prompts/blogs/intents/browse-blogs.ts
@@ -1,10 +1,11 @@
-import { Firestore } from "../../../shared/firestore";
-import { BlogPostService } from "../../../services/blog-post-service";
-import { showOrBrowseBlogPosts } from "../responses";
+import { Parameters } from './../../../dialogflow-constants';
+import { Firestore } from '../../../shared/firestore';
+import { BlogPostService } from '../../../services/blog-post-service';
+import { showOrBrowseBlogPosts } from '../responses';
 
 export async function browseBlogs(conv, params) {
   const filters = <any>{};
-  const { person } = params;
+  const person = params[Parameters.SPEAKER];
 
   if (person) {
     filters.person = person;
